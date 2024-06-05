e1a724001c9461bc96d66cfd05fcff709573354f
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-06-05 22:36:53 +0300

    Create 1051. Height Checker.py

---
+++ 1051. Height Checker.py
@@ -0,0 +1,9 @@
+heights = [1,1,4,2,1,3]
+def heightChecker(heights):
+        heightsort = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heightsort[i] != heights[i]:
+                count += 1
+        return count
+print(heightChecker(heights))
