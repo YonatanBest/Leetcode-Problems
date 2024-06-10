996ec2f9aabf8f9181d03e9b6d020e929b1b7e6e
YONATAN YISHAK YIFAT <53448650+YonatanBest@users.noreply.github.com>
2024-06-10 21:44:19 +0300

    Create 344. Reverse String.py

---
+++ 344. Reverse String.py
@@ -0,0 +1,9 @@
+s = ["h","e","l","l","o"]
+def reverseString(s):
+    """ The code starts here."""
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    s.reverse()
+    """ The code ends here."""
+print(reverseString(s))
